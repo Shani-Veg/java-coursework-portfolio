beb5e955213c0c1d1b6167470b9c6932003d1bce
Shani-Veg <shve39@gmail.com>
2026-06-02 14:06:50 +0300

    Update schema.sql

--- calendar-scheduler/src/main/java/schema.sql
+++ calendar-scheduler/src/main/java/schema.sql
@@ -5,3 +5,5 @@ CREATE TABLE appointments (
     day_num INT NOT NULL,
     appointment_text TEXT NOT NULL
 );
+
+ALTER TABLE appointments ADD CONSTRAINT unique_date UNIQUE (year_num, month_num, day_num);
